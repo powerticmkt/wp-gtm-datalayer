321ec1c79e9e7a65c3a0f5bd1ce325ff484fd557
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2017-02-12 23:53:09 -0200

    Bump 0.9.4

--- wp-gtm-datalayer.php
+++ wp-gtm-datalayer.php
@@ -3,7 +3,7 @@
  * Plugin Name: Google Tag Manager DataLayer
  * Plugin URI: https://github.com/luizeof/wp-gtmdl-datalayer
  * Description: Generate a Google Tag Manager DataLayer with a lot of data
- * Version: 0.9.3
+ * Version: 0.9.4
  * Author: luizeof
  * Author URI: http://luizeof.com.br
  * License: GPL3
